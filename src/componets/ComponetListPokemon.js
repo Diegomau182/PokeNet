afd9019427acac9f5a7b0b859e8735d6758c9a37
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2021-03-02 22:56:09 -0600

    Creacion del componente para el listado de pokemon

---
+++ src/componets/ComponetListPokemon.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet,View, Text} from 'react-native';
+
+const ComponetListPokemon = ([
+    nombrePokemon,
+    idPokemon,
+    callback
+]
+) =>{
+    return (
+        <View>
+            <Text>Pokemon</Text>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ComponetListPokemon;
