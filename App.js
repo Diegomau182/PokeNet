da6a92e90f298a87d2a96c38285fd49b02ab94ac
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2021-03-02 19:42:28 -0600

    Muestra los datos obtenido de pokeapi

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import fetchPokemon from './src/api';
+
 
 export default function App() {
+  const getPokemon = async () => {
+    const pokemon = await fetchPokemon(1);
+    console.log(pokemon);
+  }
+  getPokemon();
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
