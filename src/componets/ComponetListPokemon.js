4bcb1e820b758bd36d5f277273369ab26cc25a71
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2021-03-03 09:47:30 -0600

    Trae el componente de la lista de generaciones de pokemon

--- src/componets/ComponetListPokemon.js
+++ src/componets/ComponetListPokemon.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import {StyleSheet,View, Text} from 'react-native';
+import {StyleSheet,View, Text,Image} from 'react-native';
 
-const ComponetListPokemon = ([
-    nombrePokemon,
-    idPokemon,
+const ComponetListPokemon = ({
+    generacion,
     callback
-]
+}
 ) =>{
+    const imgPokemon="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
     return (
         <View>
-            <Text>Pokemon</Text>
+            {generacion.map((pokemon) => ( 
+                <View>
+                    <View style={styles.marcoPokemon}>
+                        <Image source={{uri:`${imgPokemon}${pokemon.id}.png`}}
+                            style={styles.pokeImagen}>
+                        </Image>
+                    </View>
+                    <Text key={pokemon}>{pokemon.name}</Text>
+                </View>
+            ))}
         </View>
 
     )
 }
 
 const styles = StyleSheet.create({
+    
+    pokeImagen: {
+    flex: 1,
+    resizeMode: "stretch",
+  },
+  marcoPokemon:{
+    flex: 1,
+    backgroundColor: "#e8cc57",
+    height: 155,
+    marginLeft: -20,
+    marginTop: 30,
+    marginRight: -20,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  
 
 });
 
