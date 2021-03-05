ce353e441c83f62a3ff09f3f0dcada66989774d1
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2021-03-05 08:40:20 -0600

    Se traslado los componentes a una pantalla a parte se da comienzo al login

--- App.js
+++ App.js
@@ -1,48 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import fetchPokemon from './src/api';
-import ComponetListPokemon from './src/componets/ComponetListPokemon';
-
+import React from 'react';
+import PokeDexScreen from './src/screens/PokeDexScreen';
 
 export default function App() {
-  const [pokemon, setPokemon] = useState([]);
-
-  useEffect(() => {
-  const getPokemon = async () => {
-    let generacion = [];
-    for (let i = 1; i <= 151; i++) {
-      if (i != 137) {
-        const pokemon = await fetchPokemon(i);
-        generacion = generacion.concat(pokemon);
-        
-      }
-      else
-      {
-
-      }
-    }
-      setPokemon(generacion)
-  }
-  getPokemon();
-},[])
-
-  console.log(pokemon);
-
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <ComponetListPokemon generacion ={pokemon}/>
-    </View>
-  );
+  return <PokeDexScreen/>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
