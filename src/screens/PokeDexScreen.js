962015f31e92449e8b13ff32fe1f6a8c3c67a28f
Diegomau182 <61211670+Diegomau182@users.noreply.github.com>
2021-03-05 08:42:15 -0600

    Se creo la pantalla donde entraran los componentes de la pokedex

---
+++ src/screens/PokeDexScreen.js
@@ -0,0 +1,47 @@
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import fetchPokemon from '../api';
+import ComponetListPokemon from '../componets/ComponetListPokemon';
+
+
+const PokeDexScreen= () => {
+
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+  const getPokemon = async () => {
+    let generacion = [];
+    for (let i = 1; i <= 151; i++) {
+      if (i != 137) {
+        const pokemon = await fetchPokemon(i);
+        generacion = generacion.concat(pokemon);
+        
+      }
+      else
+      {
+
+      }
+    }
+      setPokemon(generacion)
+  }
+  getPokemon();
+},[])
+
+  console.log(pokemon);
+
+  return (
+    <View style={styles.container}>
+      <ComponetListPokemon generacion ={pokemon}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+export default PokeDexScreen;
