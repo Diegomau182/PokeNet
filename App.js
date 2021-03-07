3301c289ba9dd99652726786c31656e2dd568a2a
dleann <dinaleanng@gmail.com>
2021-03-07 13:52:11 -0600

    Implemntado las llaves de Firebase para la navegacion de la App

--- App.js
+++ App.js
@@ -1,6 +1,43 @@
-import React from 'react';
-import PokeDexScreen from './src/screens/PokeDexScreen';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { ThemeProvider } from "react-native-elements";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Signin from "./src/screens/Singin";
+import Signup from "./src/screens/Singup";
+import Home from "./src/screens/Home";
+import theme from "./src/componets/theme";
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return <PokeDexScreen/>;
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Signin"
+              component={Signin}
+              initialParams={{ userCreated: false }}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Signup" component={Signup} />
+            <Stack.Screen name="Home" component={Home} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </ThemeProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- enviroment.js
+++ enviroment.js
@@ -2,20 +2,20 @@ import Constant from "expo-constants";
 
 const ENV = {
     dev: {
+        apiKey: "AIzaSyB-PF8WdFd6R2cA2b9RTRzSGdqgqzWXL9c",
+        authDomain: "pokenet-pokemon-app.firebaseapp.com",
+        projectId: "pokenet-pokemon-app",
         storageBucket: "pokenet-pokemon-app.appspot.com",
         messagingSenderId: "624399918774",
-        appId: "1:624399918774:web:8a08493649f87f0073a817",
-        webKeyApi:"AIzaSyB-PF8WdFd6R2cA2b9RTRzSGdqgqzWXL9c",
-        outhDomain:"pokenet-pokemon-app",
-        projectId:"624399918774",
+        appId: "1:624399918774:web:8a08493649f87f0073a817"
     },
     production: {
-        webKeyApi:"AIzaSyB-PF8WdFd6R2cA2b9RTRzSGdqgqzWXL9c",
-        outhDomain:"pokenet-pokemon-app",
-        projectId:"624399918774",
+        apiKey: "AIzaSyB-PF8WdFd6R2cA2b9RTRzSGdqgqzWXL9c",
+        authDomain: "pokenet-pokemon-app.firebaseapp.com",
+        projectId: "pokenet-pokemon-app",
         storageBucket: "pokenet-pokemon-app.appspot.com",
         messagingSenderId: "624399918774",
-        appId: "1:624399918774:web:8a08493649f87f0073a817",
+        appId: "1:624399918774:web:8a08493649f87f0073a817"
     }
 };
 
